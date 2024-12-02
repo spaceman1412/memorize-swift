2d359dbe4ed9be33c3de686b51e997cb3b38ffb6
spaceman1412 <spacemannormal@gmail.com>
2024-12-02 10:12:47 +0700

    Initial Commit

---
+++ SetCard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SetCard
+//
+//  Created by H470-088 on 2/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SetCard/SetCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetCardApp.swift
+//  SetCard
+//
+//  Created by H470-088 on 2/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SetCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
